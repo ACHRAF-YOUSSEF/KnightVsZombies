efbabcf95b34745b772b31089b357e2e5efa3225
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2022-10-16 16:16:11 +0100

    Update level_editor.py

--- LevelEditor_0/level_editor.py
+++ LevelEditor_0/level_editor.py
@@ -31,10 +31,10 @@ scroll_speed = 1
 
 
 #load images
-pine1_img = pygame.image.load('../LevelEditor_0/img/Background/pine1.png').convert_alpha()
-pine2_img = pygame.image.load('../LevelEditor_0/img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('../LevelEditor_0/img/Background/mountain.png').convert_alpha()
-sky_img = pygame.image.load('../LevelEditor_0/img/Background/sky_cloud.png').convert_alpha()
+pine1_img = pygame.image.load('./LevelEditor_0/img/Background/pine1.png').convert_alpha()
+pine2_img = pygame.image.load('./LevelEditor_0/img/Background/pine2.png').convert_alpha()
+mountain_img = pygame.image.load('./LevelEditor_0/img/Background/mountain.png').convert_alpha()
+sky_img = pygame.image.load('./LevelEditor_0/img/Background/sky_cloud.png').convert_alpha()
 #store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
@@ -42,8 +42,8 @@ for x in range(TILE_TYPES):
 	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
 	img_list.append(img)
 
-save_img = pygame.image.load('../LevelEditor_0/img/save_btn.png').convert_alpha()
-load_img = pygame.image.load('../LevelEditor_0/img/load_btn.png').convert_alpha()
+save_img = pygame.image.load('./LevelEditor_0/img/save_btn.png').convert_alpha()
+load_img = pygame.image.load('./LevelEditor_0/img/load_btn.png').convert_alpha()
 
 
 #define colors
@@ -131,7 +131,7 @@ while run:
 	#save and load data
 	if save_button.draw(screen):
 		#save level data
-		with open(f'../Levels/level{level}_data.csv', 'w', newline='') as csvfile:
+		with open(f'./Levels/level{level}_data.csv', 'w', newline='') as csvfile:
 			writer = csv.writer(csvfile, delimiter = ',')
 			for row in world_data:
 				writer.writerow(row)
@@ -140,7 +140,7 @@ while run:
 		#load in level data
 		#reset scroll back to the start of the level
 		scroll = 0
-		with open(f'../Levels/level{level}_data.csv', newline='') as csvfile:
+		with open(f'./Levels/level{level}_data.csv', newline='') as csvfile:
 			reader = csv.reader(csvfile, delimiter = ',')
 			for x, row in enumerate(reader):
 				for y, tile in enumerate(row):
